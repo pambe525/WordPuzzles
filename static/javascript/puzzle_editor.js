12ba07b519a10009d0ac5bc1025bfc9ff437fabb
prashantambe <prashant.ambe@gmail.com>
2020-11-02 23:50:56 -0500

    Completed edit_puzzle.html editing

--- static/javascript/puzzle_editor.js
+++ static/javascript/puzzle_editor.js
@@ -1,9 +1,9 @@
 class PuzzleEditor {
 
     IDs = {
-        grid: null, title: '#page_title', desc: '#desc', shared: '#share-toggle', selectSize: '#size',
-        selectMode: '#edit-toggle',
-        modeTip: '#mode-tip', saveBtn: '#save', deleteBtn: "#delete", clueForm: '#clue-form',
+        grid: null, title: '#page_title', desc: '#desc', shared: '#share-toggle', sizeSelect: '#size',
+        sizeLable: '#size-label', modeSelect: '#mode-toggle', modeLabel: '#mode-label',
+        modeTip: '#mode-tip', saveOk: 'save-ok', saveBtn: '#save', deleteBtn: "#delete", clueForm: '#clue-form',
         clueNum: '#clue-num', clueWord: '#clue-word', clueText: '#clue-text', clueMsg: '#clue-msg',
         clueUpdateBtn: '#clue-update', clueDeleteBtn: "#clue-delete", doneBtn: "#done"
     };
