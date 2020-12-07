9c7cd58aa79759ef3798b1364074855debcf636d
prashantambe <prashant.ambe@gmail.com>
2020-12-07 08:58:53 -0500

    QUnit Runner changes

--- testing/qunit_tests/test.js
+++ testing/qunit_tests/test.js
@@ -1,7 +1,21 @@
-QUnit.module("TEST");
+QUnit.module("TEST", {
+    before: function() {
+        var elemsWithIds = getElementsWithIdsFrom("edit_puzzle.html");
+        $("#qunit-fixture").append(elemsWithIds);
+    }
+});
 
 test("check if elements exist", function(assert){
     assert.equal($("#qunit-fixture").children().length, 19);
     assert.equal($("#home").length, 1);
     assert.equal($("#save").length, 1);
+    $("#save").prop("disabled", true);
+    assert.equal($("#save").prop("disabled"), true);
+});
+
+test("check if fixture is reset", function(assert){
+    assert.equal($("#qunit-fixture").children().length, 19);
+    assert.equal($("#home").length, 1);
+    assert.equal($("#save").length, 1);
+    assert.equal($("#save").prop("disabled"), false);
 })
