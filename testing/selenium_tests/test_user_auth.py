d56d721e5b3e453dfb275685d62df9ac1f5fd374
prashantambe <prashant.ambe@gmail.com>
2022-01-31 12:16:28 -0500

    Set signup.html template with link to login

--- testing/selenium_tests/test_user_auth.py
+++ testing/selenium_tests/test_user_auth.py
@@ -15,12 +15,17 @@ class UserAuthTests(LiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_Login(self):
-        self.selenium.get(self.live_server_url + '/login')
-        self.selenium.implicitly_wait(10)
-        username_input = self.selenium.find_element(By.NAME, "btnSignIn")
-        self.assertIsNotNone(username_input)
+    def test_SignUp_with_invalid_input_displays_errors(self):
+        self.selenium.get(self.live_server_url + '/signup')
+        username_input = self.selenium.find_element(By.ID, "id_username")
+        password1_input = self.selenium.find_element(By.ID, "id_password1")
+        password2_input = self.selenium.find_element(By.ID, "id_password2")
+        email_input = self.selenium.find_element(By.ID, "id_email")
+        signin_btn = self.selenium.find_element(By.NAME, "btnSignUp")
         #username_input.send_keys('myuser')
         ##password_input = self.selenium.find_element_by_name("password")
         #password_input.send_keys('secret')
         #self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
+
+    def __enterInput(self, input_id, text):
+        self.selenium.find_element(By.ID, input_id).send_keys(text)
