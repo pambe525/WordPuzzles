c520c5db789b311fb97858f6ebbc876235520d5f
pambe525 <prashant.ambe@gmail.com>
2022-03-24 18:20:21 -0400

    Moved selenium tests for SolvePuzzleTests to its own file test_selenium_solve_puzzle.py

---
+++ testing/selenium_tests/test_selenium_solve_puzzle.py
@@ -0,0 +1,8 @@
+class SolvePuzzleTests(SeleniumTestCase):
+    user = None
+    password = 'secret_key'
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="test_user", email="user@test.com", password=self.password)
+        self.login_user(username=self.user.username, password=self.password)
+        self.other_user = User.objects.create_user(username="other_user", password=self.password)
