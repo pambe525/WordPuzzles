bceb3b9347a13e2a8464379f6e9c5c1cda108be1
pambe525 <prashant.ambe@gmail.com>
2022-03-24 13:00:03 -0400

    added create_session method to unit_test_helpers.py

--- testing/django_unit_tests/unit_test_helpers.py
+++ testing/django_unit_tests/unit_test_helpers.py
@@ -1,13 +1,13 @@
 from django.utils.timezone import now
 
-from puzzles.models import WordPuzzle
+from puzzles.models import WordPuzzle, PuzzleSession
 
 
-def create_draft_puzzle(user=None, desc=None, type=0, clues_pts=None, has_parsing=False):
+def create_draft_puzzle(editor=None, desc=None, type=0, clues_pts=None, has_parsing=False):
     if clues_pts is None: clues_pts = [1]
     n_clues = len(clues_pts)
     SUFFIX = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H'}
-    puzzle = WordPuzzle.objects.create(editor=user, type=type, desc=desc)
+    puzzle = WordPuzzle.objects.create(editor=editor, type=type, desc=desc)
     for n in range(1, n_clues + 1):
         parsing = None if not has_parsing else "Parsing for word" + SUFFIX[n]
         puzzle.add_clue(
@@ -16,11 +16,15 @@ def create_draft_puzzle(user=None, desc=None, type=0, clues_pts=None, has_parsin
     return puzzle
 
 
-def create_published_puzzle(user=None, desc=None, type=0, posted_on=now(), clues_pts=None, has_parsing=False):
-    puzzle = create_draft_puzzle(user=user, desc=desc, type=type, clues_pts=clues_pts, has_parsing=has_parsing)
+def create_published_puzzle(editor=None, desc=None, type=0, posted_on=now(), clues_pts=None, has_parsing=False):
+    puzzle = create_draft_puzzle(editor=editor, desc=desc, type=type, clues_pts=clues_pts, has_parsing=has_parsing)
     puzzle.publish(time_stamp=posted_on)
     return puzzle
 
 
 def get_full_clue_desc(clue):
     return str(clue.clue_num) + ". " + clue.get_decorated_clue_text() + " [" + str(clue.points) + " pts]"
+
+def create_session(solver=None, puzzle=None, solved_clues=None, revealed_clues=None):
+    return PuzzleSession.objects.create(solver=solver, puzzle=puzzle, solved_clue_nums=solved_clues,
+                                        revealed_clue_nums=revealed_clues)
