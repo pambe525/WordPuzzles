68a81e9150e063668f9b716f838aba629c8cd149
prashantambe <prashant.ambe@gmail.com>
2022-04-23 21:35:28 -0400

    added PuzzleSession table to admin.py

--- puzzles/admin.py
+++ puzzles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Puzzle, WordPuzzle, Clue
+from .models import Puzzle, WordPuzzle, Clue, PuzzleSession
 
 # Register your models here.
 admin.site.register(Puzzle)
 admin.site.register(WordPuzzle)
 admin.site.register(Clue)
+admin.site.register(PuzzleSession)
