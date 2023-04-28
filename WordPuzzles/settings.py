dee8299559856e42e5f4817969faf88cfee9334b
pambe525 <prashant.ambe@gmail.com>
2023-04-28 12:10:51 -0400

    Tagged as v1.0 - mysql database migration of old code

--- WordPuzzles/settings.py
+++ WordPuzzles/settings.py
@@ -79,12 +79,10 @@ WSGI_APPLICATION = 'WordPuzzles.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': str(BASE_DIR / 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': str(BASE_DIR / 'database.cnf'),
-            'init_command': "SET foreign_key_checks = 0;",
+            'init_command': "SET foreign_key_checks = 0; SET sql_mode = STRICT_TRANS_TABLES;",
         },
         'TEST': {
             'NAME': 'test_wordpuzzles'
